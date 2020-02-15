39539d82b10e38a3bfe84746b8a36518d05f9bb4
Aryan Arora <arora.aryan7@gmail.com>
2020-02-15 13:21:58 +0530

    AEXify

--- aosp_Z01R.mk
+++ aosp_Z01R.mk
@@ -29,7 +29,7 @@ TARGET_GAPPS_ARCH := arm64
 CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
